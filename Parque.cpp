5047c6c9c625a3d36bfcf20c9ade72e3f17ccbd7
Berta <nosarabs@gmail.com>
2018-06-13 17:36:45 -0600

    version 1

--- Parque.cpp
+++ Parque.cpp
@@ -9,6 +9,10 @@ Parque::~Parque() {
 }
 
 std::vector<int> Parque::busquedaExhaustiva(const int** espera, int m, int n, const int* disfrute, const int** traslado) {
+  vector<int> sigma((d.m)/2, 0);
+  int Ts = 0;
+  int Te = 0;
+  fase(d.espera, d.m, d.n, d.disfrute, d.traslado, 0, 0, sigma, 0, 1);
 
 }
 
@@ -18,4 +22,28 @@ std::vector<int> Parque::programacionDinamica(const int** espera, int m, int n, 
 
 std::vector<int> Parque::algoritmoAvido(const int** espera, int m, int n, const int* disfrute, const int** traslado) {
 
+}
+
+std::vector<int> fase(const int** espera, int m, int n, const int* disfrute, const int** traslado, int suma, int nvisitados, sigma, int i, int j) {
+
+  if (suma > Ts) {
+    return sigma;
+  }
+  if (i == 0) {
+      suma = Te + d.traslado[0][sigma[i]];
+  }
+  sigma[i] = j;
+  int contador = 1;
+  while (contador == j) {
+    ++contador;
+  }
+  j = contador;
+
+  suma = d.espera[suma][sigma[i]-1] + d.disfrute[sigma[i]-1] + d.traslado[sigma[i]][j];
+
+  if (suma + d.traslado[sigma[i]][8] <= Ts) {
+    ++nvisitas;
+    fase(const int** espera, int m, int n, const int* disfrute, const int** traslado, int suma, int nvisitados, sigma, int i+1, int j); // Hay que actualizar los parametros
+    suma = suma + d.traslado[sigma[i]-1][8];
+  }
 }
--- Parque.h
+++ Parque.h
@@ -1,16 +1,25 @@
 #ifndef PARQUE
 #define PARQUE
+#include "Datos.h"
 #include <vector>
 #include <iostream>
 using namespace std;
+
 
 class Parque {
   public:
     Parque();
     ~Parque();
 
-    std::vector<int> busquedaExhaustiva(const int** espera, int m, int n, const int* disfrute, const int** traslado);
-    std::vector<int> programacionDinamica(const int** espera, int m, int n, const int* disfrute, const int** traslado);
-    std::vector<int> algoritmoAvido(const int** espera, int m, int n, const int* disfrute, const int** traslado);
+    vector<int> busquedaExhaustiva(const int** espera, int m, int n, const int* disfrute, const int** traslado);
+    vector<int> programacionDinamica(const int** espera, int m, int n, const int* disfrute, const int** traslado);
+    vector<int> algoritmoAvido(const int** espera, int m, int n, const int* disfrute, const int** traslado);
+
+  private:
+    vector<int> fase(const int** espera, int m, int n, const int* disfrute, const int** traslado, int suma, int nvisitados, sigma<int>, int i, int j);
+    Datos d;
+    int Te;
+    int Ts;
+
 };
 #endif
